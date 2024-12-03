347a46f311e265a29878fedff1ff739a344ceddb
Soc Basillote <dicecepe@gmail.com>
2024-12-03 19:34:12 +0800

    updated

--- restaurantdjango/djangodelights/views.py
+++ restaurantdjango/djangodelights/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 
-# Create your views here.
+def index(request):
+    context = {
+        'ingredients': Ingredient.objects.all(),
+        'menu_items': MenuItem.objects.all(),
+        'recipe_requirements': RecipeRequirement.objects.all(),
+        'purchases': Purchase.objects.all(),
+    }
+    return render(request, 'djangodelights/index.html', context)
